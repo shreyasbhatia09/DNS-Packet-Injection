24b136efe8e2062beda1b13fac611c4724a3f041
shreyas <shreyasbhatia09@gmail.com>
2017-12-09 21:20:59 -0500

    Updated dnsdetect taking only host IP's

--- dns-inject/Source/dnsdetect.py
+++ dns-inject/Source/dnsdetect.py
@@ -1,44 +1,91 @@
-#! /usr/bin/python
+# !/usr/bin/env python
+# title           : dnsdetect.py
+# description     : script to inject dns packets
+# author          : Shreyas Bhatia
+# date            : Dec 9th 2017
+# usage           : python dnsdetect.py -h [hostnames] -i [interface]
+# notes           :
+# python_version  :2.7
+# ==============================================================================
 
+# Imports!
 from scapy.all import *
 from optparse import OptionParser
-from scapy.utils import PcapWriter
 import netifaces
+import time
+import datetime
 
 
+# References
 # https://stackoverflow.com/questions/7574092/python-scapy-wrpcap-how-do-you-append-packets-to-a-pcap-file
+
 class DNSDetect:
     def __init__(self, interface, pcap_file, expression):
         self.interface = interface
-
+        # Get default interface if not given
         if self.interface is None:
             interfaces = netifaces.interfaces()
             interfaces.remove("lo")
             self.interface = str(interfaces[0])
         self.pcap_file = pcap_file
         self.expression = expression
-        self.pcap_dump = PcapWriter(self.pcap_file, append=True, sync=True)
 
+    # Begin detection.
     def start_detection(self):
         packets = []
+
+        # Callback for the packet sniffer
         def callback(packet):
             if packet.haslayer(DNS) and packet.haslayer(DNSRR) and packet[DNS].qr == 1:
                 for prev_packet in packets:
-                    if prev_packet[DNS].id  == packet[DNS].id and \
-                    packet[DNS].qd.qname == prev_packet[DNS].qd.qname and\
-                    packet[1][DNSRR].rdata != prev_packet[1][DNSRR].rdata:
-                        print "Spoofing detected"
-                        print packet.summary()
-                        self.pcap_dump.write(packet)
+                    if prev_packet[DNS].id == packet[DNS].id and \
+                                    packet[DNS].qd.qname == prev_packet[DNS].qd.qname:
+
+                        # Checking for the list of responses from the dns query.
+                        # The responses should be the same if its not spoofed
+
+                        rdata_pack = [packet[DNS].an[i].rdata
+                                      for i in range(packet[DNS].ancount)
+                                      if packet[DNS].an[i].type == 1]
+
+                        rdata_prev_pack = [prev_packet[DNS].an[i].rdata
+                                           for i in range(prev_packet[DNS].ancount)
+                                           if prev_packet[DNS].an[i].type == 1]
+
+                        intersection = set(rdata_pack).intersection(rdata_prev_pack)
+                        # If there are conflicing IP's then we detect a spoof attack
+                        if intersection == set([]):
+                            ts = time.time()
+                            msg = datetime.datetime.fromtimestamp(ts).strftime(
+                                "%Y%m%d %H:%M:%S.%f") + " DNS poisoning attempt" \
+                                  + "\n" + \
+                                  "TXID " + str(packet[DNS].id) + " Request " + str(packet[DNS].qd.qname.rstrip('.')) \
+                                  + "\n" + \
+                                  "Answer1  " + str(rdata_pack) \
+                                  + "\n" + \
+                                  "Answer2 " + str(rdata_prev_pack)
+                            print msg
+                            return
                 packets.append(packet)
 
+        # Callback ends here
 
-        sniff(
-            filter=self.expression,
-            prn=callback,
-            store=0,
-            iface=self.interface
-        )
+        # Let the sniffing begin!
+        if self.pcap_file is not None:
+            sniff(
+                offline=self.pcap_file,
+                filter=self.expression,
+                prn=callback,
+                store=0,
+                iface=self.interface
+            )
+        else:
+            sniff(
+                filter=self.expression,
+                prn=callback,
+                store=0,
+                iface=self.interface
+            )
 
 
 if __name__ == '__main__':
@@ -50,9 +97,9 @@ if __name__ == '__main__':
     (options, args) = optparser.parse_args()
     expression = " ".join(args)
 
-    dnsdetect = DNSDetect(options.interface,
-                          options.pcapfile,
-                          expression
-                          )
-
-    dnsdetect.start_detection()
+    dns_detect = DNSDetect(options.interface,
+                           options.pcapfile,
+                           expression
+                           )
+    print "Starting DNS Spoof attack detection"
+    dns_detect.start_detection()
